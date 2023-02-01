d20ea4604f58e91b4cb8a08e825c8f12f933a325
doraemon123453@gmail.com <doraemon123453@gmail.com>
2023-02-02 01:06:51 +0900

    ガチャ見たいなの作成

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
-  <Tutorial />
+  <!-- <Tutorial /> -->
+  <!-- <Gatya /> -->
+  <Iraira />
 </template>
 <script setup>
 import Tutorial from './components/tutorial.vue'
+import Gatya from './components/Gatya.vue'
+import Iraira from './components/Iraira.vue'
 </script>
 
 <style scoped>
